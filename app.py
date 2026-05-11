f388d4576b18c7945f96207a97f46054910128e9
debolinaofficial2 <debolinaofficial2@gmail.com>
2026-05-11 06:29:10 +0000

    fix

--- app.py
+++ app.py
@@ -1456,6 +1456,8 @@ if st.session_state.page == "results":
         )
 
         top_section = f"""
+        <link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         <div style="
         position:relative;
         margin-bottom:20px;
